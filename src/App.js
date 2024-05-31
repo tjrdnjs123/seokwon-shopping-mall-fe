c6336c0283ee6f46387f245852f3d76a6baa8aab
Byeong Min Cho <merrybmc@gmail.com>
2024-05-31 12:11:38 +0900

    react 18ver source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { userActions } from "./action/userAction";
+import "./style/common.style.css";
+import AppLayout from "./Layout/AppLayout";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppLayout>
+        <AppRouter />
+      </AppLayout>
     </div>
   );
 }
